2c38a31019f56e5361570886d9dbd88ac2b99a72
Juan Pablo Sala <juanpablosala@live.com.ar>
2020-06-21 01:10:16 -0300

    agrego boton en formulario

--- repositorio-demo/Form1.cs
+++ repositorio-demo/Form1.cs
@@ -16,5 +16,10 @@ namespace repositorio_demo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("todo ok");
+        }
     }
 }
